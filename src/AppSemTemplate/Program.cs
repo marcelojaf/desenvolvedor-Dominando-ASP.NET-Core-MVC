40b6e4a668e80246b1b48e24046a2cf97429b90f
Marcelo Amador <marcelojaf@gmail.com>
2024-03-19 17:10:23 -0300

    DbContext e Model

--- src/AppSemTemplate/Program.cs
+++ src/AppSemTemplate/Program.cs
@@ -1,6 +1,12 @@
+using AppSemTemplate.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
