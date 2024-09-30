8b7ffc8c53824569b87b3653a097db0c13c5ccae
mjamadorarthrex <marcelo.armador@contractor.arthrex.com>
2024-09-30 14:46:14 -0300

    Projeto ASP.NET Core Empty

--- src/AppSemTemplate/Program.cs
+++ src/AppSemTemplate/Program.cs
@@ -1,21 +1,6 @@
-using AppSemTemplate.Data;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 var app = builder.Build();
 
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
