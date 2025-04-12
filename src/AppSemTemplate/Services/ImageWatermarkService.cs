08e7be037cf174d520a636759acabad58f3ac579
marcelojaf <marcelojaf@gmail.com>
2025-04-11 22:58:52 -0300

    Atualizando com ImageSharp

--- src/AppSemTemplate/Services/ImageWatermarkService.cs
+++ src/AppSemTemplate/Services/ImageWatermarkService.cs
@@ -1,69 +1,69 @@
-﻿
-using AppSemTemplate.Configuration;
+﻿using AppSemTemplate.Configuration;
 using AppSemTemplate.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
-using System.Drawing;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Processing;
 
 namespace AppSemTemplate.Services
 {
+    /// <summary>
+    /// Serviço que processa imagens de produtos em background e aplica uma marca d'água.
+    /// </summary>
     public class ImageWatermarkService : BackgroundService
     {
         private readonly IServiceProvider _serviceProvider;
         private readonly IWebHostEnvironment _webHostEnvironment;
-        private readonly string _waterMark;
+        private readonly string _watermarkPath;
 
         public ImageWatermarkService(IServiceProvider serviceProvider, IWebHostEnvironment webHostEnvironment)
         {
             _serviceProvider = serviceProvider;
             _webHostEnvironment = webHostEnvironment;
-            _waterMark = Path.Combine(_webHostEnvironment.WebRootPath, "images/logo_watermark.png");
+            _watermarkPath = Path.Combine(_webHostEnvironment.WebRootPath, "images", "logo_watermark.png");
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                using(var scope = _serviceProvider.CreateScope())
+                using var scope = _serviceProvider.CreateScope();
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                var configuration = scope.ServiceProvider.GetRequiredService<IOptions<ApiConfiguration>>();
+
+                var produtosNaoProcessados = await dbContext.Produtos
+                    .Where(p => !p.Processado)
+                    .ToListAsync();
+
+                foreach (var produto in produtosNaoProcessados)
                 {
-                    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                    var configuration = scope.ServiceProvider.GetRequiredService<IOptions<ApiConfiguration>>();
-                    
-                    // Obter produtos não processados
-                    var produtosNaoProcessados = await dbContext.Produtos
-                        .Where(p => !p.Processado)
-                        .ToListAsync();
-
-                    foreach (var produto in produtosNaoProcessados)
+                    try
                     {
-                        try
+                        var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", produto.Imagem);
+                        var newImagePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", "prod_" + produto.Imagem);
+
+                        using Image image = await Image.LoadAsync(imagePath, stoppingToken);
+                        using Image watermark = await Image.LoadAsync(_watermarkPath, stoppingToken);
+
+                        image.Mutate(ctx =>
                         {
-                            var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", produto.Imagem);
-                            var newImagePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", "prod_" + produto.Imagem);
+                            ctx.Resize(381, 499);
+                            var position = new Point(image.Width - watermark.Width - 10, image.Height - watermark.Height - 10);
+                            ctx.DrawImage(watermark, position, 1f);
+                        });
 
-                            using (Bitmap watermarkImage = new Bitmap(_waterMark))
-                            using (Bitmap imagemOriginal = new Bitmap(imagePath))
-                            {
-                                using (Bitmap resizedImage = new Bitmap(381,499))
-                                using (var graphic = Graphics.FromImage(resizedImage))
-                                {
-                                    graphic.DrawImage(imagemOriginal, 0, 0, 381, 499);
-                                    var point = new Point(resizedImage.Width - 180, resizedImage.Height - 80);
+                        await image.SaveAsync(newImagePath, new PngEncoder(), stoppingToken);
 
-                                    graphic.DrawImage(watermarkImage, point);
-
-                                    resizedImage.Save(newImagePath);
-                                }
-                            }
-                            produto.Processado = true;
-                        }
-                        catch (Exception ex)
-                        {
-                        }
+                        produto.Processado = true;
                     }
-                    await dbContext.SaveChangesAsync();
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
                 }
 
+                await dbContext.SaveChangesAsync();
                 await Task.Delay(1000, stoppingToken);
             }
         }
